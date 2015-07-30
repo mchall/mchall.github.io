294a46af99b24fee30e1ae26342405de80b9906c
Mike Hall <mcsyko@gmail.com>
2015-07-30 15:16:19 +0200

    Animations

--- js/animations.js
+++ js/animations.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    // You are in mobile browser
+    //No animations for mobile
   }
   else{
-    $('#portfolio .title').css('opacity', 0).one('inview', function(isInView) {
-            if (isInView) {$(this).addClass('animated fadeInUp delay1s');}
-        });
-    $('#portfolio .portfolio-item').css('opacity', 0).one('inview', function(isInView) {
-        if (isInView) {$(this).addClass('animated fadeInUp delay1s');}
-    });
+      $('#portfolio .title').css('opacity', 0).one('inview', function(isInView) {
+          if (isInView) {$(this).addClass('animated fadeInDown');}
+      });
+      $('#portfolio .portfolio-item').css('opacity', 0).one('inview', function(isInView) {
+          if (isInView) {$(this).addClass('animated fadeInRight');}
+      });
   }
 });
