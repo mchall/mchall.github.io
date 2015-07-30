d195ec966f8685c74b7f3410f0a3aaea2a86cc7f
Mike Hall <mcsyko@gmail.com>
2015-07-30 15:24:12 +0200

    Animations

--- js/animations.js
+++ js/animations.js
@@ -9,5 +9,22 @@ $( document ).ready(function() {
       $('#portfolio .portfolio-item').css('opacity', 0).one('inview', function(isInView) {
           if (isInView) {$(this).addClass('animated fadeInRight');}
       });
+
+      $('#about .title').css('opacity', 0).one('inview', function(isInView) {
+          if (isInView) {$(this).addClass('animated fadeInDown');}
+      });
+      $('#about .about-text').css('opacity', 0).one('inview', function(isInView) {
+          if (isInView) {$(this).addClass('animated fadeInRight');}
+      });
+      $('#about .download').css('opacity', 0).one('inview', function(isInView) {
+          if (isInView) {$(this).addClass('animated fadeInUp');}
+      });
+
+      $('#contact .title').css('opacity', 0).one('inview', function(isInView) {
+          if (isInView) {$(this).addClass('animated fadeInDown');}
+      });
+      $('#contact form').css('opacity', 0).one('inview', function(isInView) {
+          if (isInView) {$(this).addClass('animated fadeInRight');}
+      });
   }
 });
