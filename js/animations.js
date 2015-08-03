773b3a6a5499ac6c0f9857c4a979e0eeee7a5408
Mike Hall <mcsyko@gmail.com>
2015-08-03 10:51:33 +0200

    Indent

--- js/animations.js
+++ js/animations.js
@@ -3,42 +3,42 @@ $( document ).ready(function() {
     //No animations for mobile
   }
   else{
-      $('#profile .title').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInDown');}
-      });
-      $('#profile img').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInRight');}
-      });
+    $('#profile .title').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInDown');}
+    });
+    $('#profile img').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInRight');}
+    });
 
-      $('#portfolio .title').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInDown');}
-      });
-      $('#portfolio .portfolio-item').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInRight');}
-      });
+    $('#portfolio .title').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInDown');}
+    });
+    $('#portfolio .portfolio-item').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInRight');}
+    });
 
-      $('#about .title').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInDown');}
-      });
-      $('#about .about-text').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInRight');}
-      });
-      $('#about .download').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInUp');}
-      });
+    $('#about .title').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInDown');}
+    });
+    $('#about .about-text').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInRight');}
+    });
+    $('#about .download').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInUp');}
+    });
 
-      $('#contact .title').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInDown');}
-      });
-      $('#contact form').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInRight');}
-      });
+    $('#contact .title').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInDown');}
+    });
+    $('#contact form').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInRight');}
+    });
 
-      $('#footer .location').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInLeft');}
-      });
-      $('#footer .credits').css('opacity', 0).one('inview', function(isInView) {
-          if (isInView) {$(this).addClass('animated fadeInRight');}
-      });
+    $('#footer .location').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInLeft');}
+    });
+    $('#footer .credits').css('opacity', 0).one('inview', function(isInView) {
+      if (isInView) {$(this).addClass('animated fadeInRight');}
+    });
   }
 });
